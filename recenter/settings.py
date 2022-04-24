9fc6ca7b17bfd49464bd12d810fb75161a21cee8
Sergey Bykov (from recenter) <derter@kitendo.ru>
2022-04-24 18:04:46 +0300

    removed django-modeltranslation from settings

--- recenter/settings.py
+++ recenter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Application definition
 
 INSTALLED_APPS = [
-    'modeltranslation',
+    #'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
